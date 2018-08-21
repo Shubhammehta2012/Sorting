a4147229c924738263288252cf12355b7c491e56
Shubham Mehta <31146862+Shubhammehta2012@users.noreply.github.com>
2018-08-21 02:41:16 -0700

    Update Insertion_Sort.py

--- Insertion_Sort.py
+++ Insertion_Sort.py
@@ -1,11 +1,6 @@
 
-# coding: utf-8
 
 # # Insertion Sort
-
-# In[1]:
-
-
 def insertion_sort(array):
     global iterations
     iterations = 0
@@ -14,20 +9,15 @@ def insertion_sort(array):
         for j in range(i - 1, -1, -1):
             iterations += 1
             if array[j] > current_value:
-                array[j], array[j + 1] = array[j + 1], array[j] # swap
+                array[j], array[j + 1] = array[j + 1], array[j] 
             else:
                 array[j + 1] = current_value
                 break
 
 
-# In[2]:
-
-
-# elements are already sorted
 array = [i for i in range(1, 20)]
 
 insertion_sort(array)
-# 20 ALREADY sorted elements need 18 iterations approx = n
 print(array)
 print(iterations)
 
