2913e04a6158dbcdeb0651607a767d4f63c03028
Shubham Mehta <31146862+Shubhammehta2012@users.noreply.github.com>
2018-08-21 02:40:42 -0700

    Update Bubble_Sort.py

--- Bubble_Sort.py
+++ Bubble_Sort.py
@@ -1,13 +1,4 @@
 
-# coding: utf-8
-
-# # Bubble Sort
-
-# In[2]:
-
-
-# optimized bubble sort does not increase or decrease asymtotic notations
-# however number of iterations can be reduced to some extent
 def optimized_bubble_sort(array):
     global iterations
     iterations = 0
@@ -18,19 +9,11 @@ def optimized_bubble_sort(array):
             if array[j] > array[j + 1]:
                 array[j], array[j + 1] = array[j + 1], array[j]
                 swapped = True
-        # if no swapping is performed that means sorting is complete
-        # hence break out of the loop
         if not swapped:          
             break
 
-
-# In[3]:
-
-
-# elements are already sorted
 array = [i for i in range(1, 20)]
 
 optimized_bubble_sort(array)
-# 20 ALREADY sorted elements need 18 iterations approx = n
 print(iterations)
 
